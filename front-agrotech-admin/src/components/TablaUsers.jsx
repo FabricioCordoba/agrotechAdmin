2de595faff1c74f655db058bb2e8e06c8cd41d37
fabricio <fabricio.cbe@gmail.com>
2024-06-27 19:17:11 -0300

    se estila tabla usuarios y se agrega paginacion

--- front-agrotech-admin/src/components/TablaUsers.jsx
+++ front-agrotech-admin/src/components/TablaUsers.jsx
@@ -3,13 +3,14 @@ import { updateUserById, deleteUser } from "../service/userService";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
+import "../styles/tablaUsers.css"
 
 function TablaUser({ clients }) {
     const { user } = useContext(UserContext);
     const navigate = useNavigate();
 
     const modalDeleteRef = useRef(null);
-    const modalEditRef = useRef(null)
+    const modalEditRef = useRef(null);
 
     const [clientUpdate, setClientUpdate] = useState({
         idUser: user.idUser,
@@ -20,6 +21,9 @@ function TablaUser({ clients }) {
     });
 
     const [filtroApellido, setFiltroApellido] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const clientsPerPage = 10;
+    const totalPages = Math.ceil(clients.length / clientsPerPage);
 
     function handleChangeEditClient(e) {
         e.preventDefault();
@@ -58,84 +62,113 @@ function TablaUser({ clients }) {
         setFiltroApellido(e.target.value.toLowerCase());
     };
 
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
     const filteredClients = clients.filter(client => client.lastname.toLowerCase().startsWith(filtroApellido));
+    const paginatedClients = filteredClients.slice((currentPage - 1) * clientsPerPage, currentPage * clientsPerPage);
 
     return (
         <>
-            <input type="text" placeholder="Buscar por apellido..." value={filtroApellido} onChange={handleFilterByLastName} />
-            <table className="clients-table">
-                <thead>
-                    <tr>
-                        <th>Apellido</th>
-                        <th>Nombre</th>
-                        <th>Telefono</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredClients.map((user) => (
-                        <tr key={user.idUser}>
-                            <td>{user.lastname}</td>
-                            <td>{user.name}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.email}</td>
-                            <td>
-                                <button className='button-edit' onClick={() => handleEditClick(user)}><AiFillEdit /></button>
-                            </td>
-                            <td>
-                                <button onClick={() => handleDeleteClick(user)}><AiFillDelete /></button>
-                            </td>
-                            <td>
-                                <button onClick={() => purchases(user.idUser)}>Historial de Compras</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            {clientUpdate && (
-                <div className='edit-form'>
-                    <dialog ref={modalEditRef} id="modalEdit">
-                        <h2>Editar cliente</h2>
-                        <form action="" method="dialog" id="form">
-                            <input
-                                type="text"
-                                name="name"
-                                value={clientUpdate.name}
-                                onChange={handleChangeEditClient}
-                            />
-                            <input
-                                type="text"
-                                name="lastname"
-                                value={clientUpdate.lastname}
-                                onChange={handleChangeEditClient}
-                            />
-                            <input
-                                type="text"
-                                name="phone"
-                                value={clientUpdate.phone}
-                                onChange={handleChangeEditClient}
-                            />
-                            <input
-                                type="text"
-                                name="email"
-                                value={clientUpdate.email}
-                                onChange={handleChangeEditClient}
-                            />
-                            <button onClick={handleSaveClick}>Guardar</button>
-                            <button>Cancelar</button>
-                        </form>
-                    </dialog>
+            <div className='general-tabla-users'>
+                <div className='table-container'>
+                    <input
+                    className='input-buscar'
+                        type="text"
+                        placeholder="Buscar por apellido..."
+                        value={filtroApellido}
+                        onChange={handleFilterByLastName}
+                    />
+                    <table className="clients-table">
+                        <thead>
+                            <tr>
+                                <th>Apellido</th>
+                                <th>Nombre</th>
+                                <th>Teléfono</th>
+                                <th>Email</th>
+                                <th>Editar</th>
+                                <th>Eliminar</th>
+                                <th>Historial de compra</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {paginatedClients.map((user) => (
+                                <tr key={user.idUser}>
+                                    <td>{user.lastname}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <button className='button-edit' onClick={() => handleEditClick(user)}><AiFillEdit /></button>
+                                    </td>
+                                    <td>
+                                        <button className='button-delete' onClick={() => handleDeleteClick(user)}><AiFillDelete /></button>
+                                    </td>
+                                    <td>
+                                        <button className='button-purchase' onClick={() => purchases(user.idUser)}>Historial de Compras</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <div className="pagination">
+                        <button onClick={handlePreviousPage} disabled={currentPage === 1}>Anterior</button>
+                        <span>{currentPage} de {totalPages}</span>
+                        <button onClick={handleNextPage} disabled={currentPage === totalPages}>Siguiente</button>
+                    </div>
                 </div>
-            )}
-
-            {clientUpdate && (
-                <dialog ref={modalDeleteRef} id='modalDelete'>
-                    <p>¿Desea eliminar al cliente {clientUpdate.name} {clientUpdate.lastname}?</p>
-                    <button onClick={confirmDelete}>Aceptar</button>
-                    <button onClick={cancelDeleteModal}>Cancelar</button>
-                </dialog>
-            )}
+                {clientUpdate && (
+                    <div className='edit-form'>
+                        <dialog ref={modalEditRef} id="modalEdit">
+                            <h2>Editar cliente</h2>
+                            <form action="" method="dialog" id="form">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={clientUpdate.name}
+                                    onChange={handleChangeEditClient}
+                                />
+                                <input
+                                    type="text"
+                                    name="lastname"
+                                    value={clientUpdate.lastname}
+                                    onChange={handleChangeEditClient}
+                                />
+                                <input
+                                    type="text"
+                                    name="phone"
+                                    value={clientUpdate.phone}
+                                    onChange={handleChangeEditClient}
+                                />
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={clientUpdate.email}
+                                    onChange={handleChangeEditClient}
+                                />
+                                <button className='btn-edit-guardar-users' onClick={handleSaveClick}>Guardar</button>
+                                <button className='btn-edit-cancelar-users'>Cancelar</button>
+                            </form>
+                        </dialog>
+                    </div>
+                )}
+                {clientUpdate && (
+                    <dialog ref={modalDeleteRef} id='modalDelete'>
+                        <p>¿Desea eliminar al cliente {clientUpdate.name} {clientUpdate.lastname}?</p>
+                        <button className='btn-delete-aceptar' onClick={confirmDelete}>Aceptar</button>
+                        <button className='btn-delete-cancelar' onClick={cancelDeleteModal}>Cancelar</button>
+                    </dialog>
+                )}
+            </div>
         </>
     );
 }
