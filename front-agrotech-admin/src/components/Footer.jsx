1a830f9cbdeb695e53aa1c91df0707f37ba17104
fabricio <fabricio.cbe@gmail.com>
2024-07-01 09:40:47 -0300

    e estila footer

--- front-agrotech-admin/src/components/Footer.jsx
+++ front-agrotech-admin/src/components/Footer.jsx
@@ -1,23 +1,38 @@
-
-import { CiInstagram } from "react-icons/ci";
-import { AiOutlineFacebook } from "react-icons/ai";
-import { LiaWhatsappSquare } from "react-icons/lia";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-
+import { FaLocationDot,  FaPhone  } from "react-icons/fa6";
+import '../styles/footer.css'; 
 
 function Footer() {
+  const gmailLink = "https://mail.google.com/mail/u/0/#inbox";
+
   return (
-    <footer>
-      <div className='social-icons'>
-        <CiInstagram />
-        <AiOutlineFacebook />
-        <LiaWhatsappSquare />
+    <div className="footer-container">
+      
+      <div className="footer-content">
+        <div className='social-icons'>
+          <FaInstagram /> 
+          <FaFacebook />
+          <FaWhatsapp />
+        </div>
+        <div className="contact-info">
+        <FaPhone />
+        <p className='p-footer'>0800-888-888-1</p>
+        </div>
+        <div className='contact-info'>
+          <HiOutlineMail />
+          <p className='p-footer'>Contacto:  <a href={gmailLink} target="_blank" rel="noopener noreferrer">
+          somos.agrotech@gmail.com
+        </a></p>
+        </div>
+        <div className='location-info'>
+          <FaLocationDot />
+          <p className='p-footer'>Av. Libertad. n° 1221 - Benito Juárez</p>
+        </div>
       </div>
-      <div className='contact-info'>
-        <HiOutlineMail />
-        <p className='p-footer'>agrotech@gmail.com | Benito Juarez</p>
-      </div>
-    </footer>
+    </div>
   );
 }
 
