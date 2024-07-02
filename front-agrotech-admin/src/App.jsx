9fab67edf5e85ebce7c59d2c3b7458566575745c
fabricio <fabricio.cbe@gmail.com>
2024-07-02 16:32:49 -0300

    se acomoda rutas protegidas

--- front-agrotech-admin/src/App.jsx
+++ front-agrotech-admin/src/App.jsx
@@ -1,45 +1,32 @@
-
-
-import "./App.css"
-import { Routes, Route, Router } from "react-router-dom"
-import Home from "./pages/Products"
-
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Category from "./pages/Category"
-import Products from "./pages/Products"
-import Clients from "./pages/Clients"
-import CustomerPurchases from "./pages/CustomerPurchases"
-import VentasPage from "./pages/VentasPage"
-import { RutasProtegidas } from "./components/RutasProtegidas"
-import { UserContext } from "./context/UserContext"
-import { useContext } from "react"
-
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Category from "./pages/Category";
+import Products from "./pages/Products";
+import Clients from "./pages/Clients";
+import CustomerPurchases from "./pages/CustomerPurchases";
+import VentasPage from "./pages/VentasPage";
+import { RutasProtegidas } from "./components/RutasProtegidas";
+import { UserContext } from "./context/UserContext";
+import { useContext } from "react";
 
 function App() {
-
-  const{isLoggedIn}= useContext(UserContext);
+  const { isLoggedIn } = useContext(UserContext);
 
   return (
-    <>
-      <Routes>
+    <Routes>
       <Route path="/" element={<Login />} />
-      <Route element={<RutasProtegidas isAllowed={isLoggedIn}/>}>
-
+      <Route element={<RutasProtegidas isAllowed={isLoggedIn} redirectPath="/" />}>
         <Route path="products" element={<Products />} />
         <Route path="register" element={<Register />} />
-        <Route path="products/:category" element={<Category/>} />
-        <Route path="/Clients" element={<Clients/>}/>
-        <Route path="/customer-purchases/:id" element={<CustomerPurchases/>}/>
-        <Route path="/ventas" element={<VentasPage/>}/>
-        </Route>        
-
-      </Routes>
-
-
-    </>
-  )
+        <Route path="products/:category" element={<Category />} />
+        <Route path="/Clients" element={<Clients />} />
+        <Route path="/customer-purchases/:id" element={<CustomerPurchases />} />
+        <Route path="/ventas" element={<VentasPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- front-agrotech-admin/src/context/UserContext.jsx
+++ front-agrotech-admin/src/context/UserContext.jsx
@@ -1,59 +1,60 @@
-import React, { createContext, useState, useEffect, Children } from "react";
-import { user } from "../service/user";
-import { FaDatabase } from "react-icons/fa6";
+import React, { createContext, useState, useEffect } from "react";
+import { user as userDefault } from "../service/user";
 
-export const UserContext = createContext(user);
+export const UserContext = createContext(userDefault);
 
 export const UserProvider = ({ children }) => {
+  const urlUsers = 'http://localhost:3000/user';
 
-  const urlUsers = 'http://localhost:3000/user'
+  const [clients, setClients] = useState([]);
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
+    return storedIsLoggedIn === 'true';
+  });
 
-  const [clients, setClients] = useState([])
-  const [user, setUser] = useState("null");
-  const [isLoggedIn, setIsloggenIn]= useState(false)
   const handleLogin = (loggedInUser) => {
     setUser(loggedInUser);
     localStorage.setItem('user', JSON.stringify(loggedInUser));
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "true");
   };
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-      setIsloggenIn(true);
-
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
+    if (storedUser && storedIsLoggedIn === 'true') {
+      const parsedUser = JSON.parse(storedUser);
+      setUser(parsedUser);
+      setIsLoggedIn(true);
     }
   }, []);
 
   const handleLogout = () => {
     setUser(null);
-    localStorage.removeItem('user'); 
-    
-  };  
+    setIsLoggedIn(false);
+    localStorage.removeItem('user');
+    localStorage.removeItem('isLoggedIn');
+  };
 
-  const fetchClients = async (urlUsers) => {
-      try {
-          const response = await fetch(urlUsers);
-          const data = await response.json();
-          console.log("data",data);
-
-        const clientsDb = data.filter(user=> user.rol=== "user" && user.active===true );
-
-          setClients(clientsDb);
-          console.log("data",clientsDb);
-      } catch (error) {
-          console.log(error);
-      }
+  const fetchClients = async () => {
+    try {
+      const response = await fetch(urlUsers);
+      const data = await response.json();
+      const clientsDb = data.filter(user => user.rol === "user" && user.active === true);
+      setClients(clientsDb);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
-      fetchClients(urlUsers);
-  }, [])
-
+    fetchClients();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, clients, handleLogin, handleLogout,isLoggedIn, setIsloggenIn }}>
+    <UserContext.Provider value={{ user, clients, handleLogin, handleLogout, isLoggedIn }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
